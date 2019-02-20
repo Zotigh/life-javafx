d4c82380c45a060ffc165a935b0c8a5f2f062fab
Lance Zotigh <lancezotighlz@gmail.com>
2019-02-20 16:07:10 -0700

    had to talk with dan will finish later

---
+++ src/edu/cnm/deepdive/life/Life.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.life;
+
+import java.awt.Button;
+import java.awt.ScrollPane;
+import javafx.fxml.FXML;
+
+public class Life {
+@FXML
+  private Button start;
+@FXML
+  private ScrollPane viewScroller;
+}
